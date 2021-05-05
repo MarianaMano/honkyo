70f3dbbdedc4e46439f57633f4785a1b83fc7e4e
MartaCerq <79975159+MartaCerq@users.noreply.github.com>
2021-05-05 15:41:29 +0100

    Zonas Trigger
    Criação de 3 acionadores com zonas trigger.

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+
+    [SerializeField]
+    float velocidade = 3f;
+    private Rigidbody corpoRigido;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        corpoRigido = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertial = Input.GetAxis("Vertical");
+        Vector3 movimento = new Vector3(moveHorizontal, 0.0f, moveVertial);
+        transform.position = transform.position + (movimento * velocidade * Time.deltaTime);
+        corpoRigido.AddForce(movimento * velocidade);
+
+    }
+}
