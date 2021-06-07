c61bd8bc3d3d5882c4814132c396a78130d29ca2
mariana mano <40200338@esmad.ipp.pt>
2021-06-07 09:20:20 +0100

    Mariana Ribeiro
    Ajuste tamanho inimigo;
    tentativa animação acionada por Enter;
    
    06-06

--- Assets/Scripts/Final/Coletaveis_final.cs
+++ Assets/Scripts/Final/Coletaveis_final.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class Coletaveis_final : MonoBehaviour
 {
 
-    public GameObject maca;
-    public GameObject pomba;
-    public GameObject coracao;
+    [SerializeField]
+    private Animator maca;
 
+    [SerializeField]
+    private Animator pomba;
+
+    [SerializeField]
+    private Animator coracao;
+
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PousaColetaveis")
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                maca.SetBool("playMaca", true);
+                pomba.SetBool("playPomba", true);
+                coracao.SetBool("playCoracao", true);
+            }
+        }
+        
+    }
+    */
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            maca.GetComponent<Animator>().Play("maca");
-            coracao.GetComponent<Animator>().Play("coraçao");
-            pomba.GetComponent<Animator>().Play("pomba");
+            maca.SetBool("playMaca", true);
+            pomba.SetBool("playPomba", true);
+            coracao.SetBool("playCoracao", true);
         }
-        
     }
 }
